bcfe64ff9b617cb4ea4f42b63baee449e978da38
mkkprabhu <mkkprabhu@gmail.com>
2016-11-26 16:09:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,22 +42,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'UR.html'));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'img'));
-});
+
+app.use("/css", express.static(__dirname+'/ui/css'));
+app.use("/img", express.static(__dirname+'/ui/img'));
+app.use("/js", express.static(__dirname+'/ui/js'));
+app.use("/fonts", express.static(__dirname+'/ui/fonts'));
+app.use("/scss", express.static(__dirname+'/ui/scss'));
+app.use("/less", express.static(__dirname+'/ui/less'));
+app.use("/request", express.static(__dirname+'/ui/request'));
+app.use("/partials", express.static(__dirname+'/ui/partials'));
 
 
 
-
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
